3af66476fcabc96fb0972448610da9b63fcf8cc3
divatekodand <divatekodand@wisc.edu>
2019-11-07 22:34:19 -0600

    adding fn to read bin file

--- pc_vis.py
+++ pc_vis.py
@@ -1,7 +1,12 @@
 #Point cloud visualizer
 # requirement - pip install open3d
-import numpy
+import numpy as np
 import open3d as o3d
+
+def read_pc(path):
+    # exact path to the bin file
+    pc = np.fromfile(path, dtype=np.float32).reshape((-1,4))
+    return pc
 
 def vis_pc(points):
     pcd = o3d.geometry.PointCloud()
