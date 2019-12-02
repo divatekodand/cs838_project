52c2e8755738e9922e33394ba7365713d9af05fd
divatekodand <divatekodand@wisc.edu>
2019-12-01 22:08:39 -0600

    bug fix

--- monodepth2/datasets/mono_dataset.py
+++ monodepth2/datasets/mono_dataset.py
@@ -192,7 +192,7 @@ class MonoDataset(data.Dataset):
             inputs["depth_gt"] = cv2.resize(depth_gt, (self.width, self.height), interpolation=cv2.INTER_LINEAR)
             print("after resize - ", inputs["depth_gt"].shape)
             print("height and width - ", self.height, "   ", self.width)
-            inputs["depth_gt"] = np.expand_dims(depth_gt, 0)
+            inputs["depth_gt"] = np.expand_dims(inputs["depth_gt"], 0)
 
             inputs["depth_gt"] = inputs["depth_gt"].astype(np.float32)
             print('input depth -', inputs["depth_gt"].shape)
