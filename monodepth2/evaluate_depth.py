e45cf01c46edbf738b830e2d8ad8244ea9a672d9
divatekodand <divatekodand@wisc.edu>
2019-12-07 21:25:07 -0600

    bugfix

--- monodepth2/evaluate_depth.py
+++ monodepth2/evaluate_depth.py
@@ -85,6 +85,7 @@ def evaluate(opt):
         encoder_dict = torch.load(encoder_path)
 
         if opt.depth_supervision and opt.obj3d:
+            print("kitti obj det")
             dataset = datasets.KITTIRAWDataset(opt.data_path, filenames,
                                                encoder_dict['height'], encoder_dict['width'],
                                                [0], 4, is_train=False, img_ext='.png')
