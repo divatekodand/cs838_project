559b7d21c86cc6f978a5dee1e58cd4328aaaf105
divatekodand <divatekodand@wisc.edu>
2019-12-13 00:58:03 -0600

    bug fix

--- pc_vis.py
+++ pc_vis.py
@@ -1,4 +1,4 @@
-#Point cloud visualizer
+# Point cloud visualizer
 # requirement - pip install open3d
 import numpy as np
 import open3d as o3d
@@ -9,23 +9,22 @@ import pdb
 
 def read_pc(path):
     # exact path to the bin file
-    pc = np.fromfile(path, dtype=np.float32).reshape((-1,4))
-    return pc
+    pc = np.fromfile(path, dtype=np.float64).reshape((-1, 4))
+    return pc[:, 0:3]
 
 
 def vis_pc(points):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
-    o3d.visualization.draw_geometries([pcd])	
+    o3d.visualization.draw_geometries([pcd])
 
 
 def main():
-	args = sys.argv
-	if(len(args) < 2):
-		print("Path to bin file required")
-	else:
-		vis_pc(read_pc(args[1]))
-	
+    args = sys.argv
+    if len(args) < 2:
+        print("Path to bin file required")
+    else:
+        vis_pc(read_pc(args[1]))
 
 
 if __name__ == '__main__':
